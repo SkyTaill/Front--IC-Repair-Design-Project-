62143f1a0198d52bd31e9d3a504bc0225e03b5de
maxim <vorobeichik2015@yandex.ru>
2022-11-22 01:07:24 +0300

    add js

---
+++ Figma/border.js
@@ -0,0 +1,14 @@
+
+const arrowL = document.getElementById('arrow-l');
+const arrowR = document.getElementById('arrow-r');
+
+arrowL.addEventListener('click', () => {
+
+    alert("message-L");
+
+});
+arrowR.addEventListener('click', () => {
+
+    alert("message-R");
+
+});
